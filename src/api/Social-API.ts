53b6e3c07964c832c8c61fe041d864ffdecefe52
KirillKozlo <kirillkozlogit@gmail.com>
2023-07-29 00:07:30 +0300

    10% api

---
+++ src/api/Social-API.ts
@@ -0,0 +1,56 @@
+import axios, {AxiosResponse} from "axios";
+
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.0',
+    withCredentials: true,
+    headers: {
+        'API-KEY': 'fdf6f163-b057-4eee-9b85-ec1216c43810'
+    }
+})
+export type UsersResponseType={
+    items:UserItemsResponseType[]
+    totalCount: number
+    error: null|string
+}
+export type UserItemsResponseType={
+    name: string
+    id: number
+    photos: UserPhotosResponseType
+    status: null|string
+    followed: boolean
+}
+export type UserPhotosResponseType={
+    small: null|string
+    large: null|string
+}
+export type RequestUpdateMyProfileType={
+    userId: number
+    lookingForAJob: boolean
+    lookingForAJobDescription: string
+    fullName: string
+    contacts:RequestUpdateMyProfileContactsType
+}
+export type RequestUpdateMyProfileContactsType={
+    github: null|string
+    vk: null|string
+    facebook: null|string
+    instagram: null|string
+    twitter: null|string
+    website: null|string
+    youtube: null|string
+    mainLink: null|string
+}
+export type ResponseUpdateMyProfileType={
+    resultCode: number
+    messages: [string],
+    data: {}
+}
+export const SocialAPI={
+    getUsers(){
+        return instance.get<UsersResponseType>('/users');
+    },
+    updateMyProfile(model:RequestUpdateMyProfileType){
+        return instance.put<ResponseUpdateMyProfileType,AxiosResponse<ResponseUpdateMyProfileType>,RequestUpdateMyProfileType>('/profile',model)
+    }
+
+}
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from "./Header.module.css"
+
+export const Header = () => {
+    return (
+        <div className={s.headerWrapper}>
+            <img src="https://cdn6.aptoide.com/imgs/5/d/1/5d187923c2f94bf506bc9444ddf02516_icon.png" alt="logo"/>
+        </div>
+    );
+};
