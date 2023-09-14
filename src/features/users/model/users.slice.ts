9095339a79cda3f55907504a31ef1bf90c993e8b
KirillKozlo <kirillkozlogit@gmail.com>
2023-09-14 21:49:04 +0300

    user.slice.ts full slice

--- src/features/users/model/users.slice.ts
+++ src/features/users/model/users.slice.ts
@@ -2,6 +2,8 @@ import { createSlice } from "@reduxjs/toolkit"
 import { createAppAsyncThunk } from "common/utils/create-app-async-thunk"
 import { Params, usersAPI } from "features/users/api/usersAPI"
 import { User, UtilResponse } from "common/types/apiTypes"
+import { followAPI } from "features/users/api/followAPI"
+import { ResultCode } from "common/enums/enums"
 
 const setUsers = createAppAsyncThunk<UtilResponse<User[]>, Params>(
   "users/setUsers",
@@ -10,6 +12,30 @@ const setUsers = createAppAsyncThunk<UtilResponse<User[]>, Params>(
     const res = await usersAPI.users(arg)
 
     return res.data
+  },
+)
+const followUser = createAppAsyncThunk<{ userId: number }, number>(
+  "users/followUser",
+  async (userId, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI
+    const res = await followAPI.follow(userId)
+    if (res.data.resultCode === ResultCode.Success) {
+      return { userId: userId }
+    } else {
+      return rejectWithValue({ data: res.data })
+    }
+  },
+)
+const unFollowUser = createAppAsyncThunk<{ userId: number }, number>(
+  "users/unFollowUser",
+  async (userId, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI
+    const res = await followAPI.unFollow(userId)
+    if (res.data.resultCode === ResultCode.Success) {
+      return { userId: userId }
+    } else {
+      return rejectWithValue({ data: res.data })
+    }
   },
 )
 
@@ -21,11 +47,24 @@ const slice = createSlice({
   },
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(setUsers.fulfilled, (state, action) => {
-      state.users = action.payload.items
-      state.totalCount = action.payload.totalCount
-    })
+    builder
+      .addCase(setUsers.fulfilled, (state, action) => {
+        state.users = action.payload.items
+        state.totalCount = action.payload.totalCount
+      })
+      .addCase(followUser.fulfilled, (state, action) => {
+        let user = state.users.find((el) => el.id === action.payload.userId)
+        if (user) {
+          user.followed = true
+        }
+      })
+      .addCase(unFollowUser.fulfilled, (state, action) => {
+        let user = state.users.find((el) => el.id === action.payload.userId)
+        if (user) {
+          user.followed = false
+        }
+      })
   },
 })
 export const authSlice = slice.reducer
-export const userThunk = {}
+export const userThunk = { setUsers, followUser, unFollowUser }
